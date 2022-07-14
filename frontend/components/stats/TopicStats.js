f83b9110da42dbb1487e345add61b21abea43dfc
shinjini25 <shinjinibh2503@gmail.com>
2022-07-14 19:22:09 +0530

    user applied jobs display done

--- frontend/components/stats/TopicStats.js
+++ frontend/components/stats/TopicStats.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import Loader from "../layouts/Loader";
+
+import JobContext from "../../context/JobContext";
+import { toast } from "react-toastify";
 
 const TopicStats = () => {
+    const [topic, setTopic] = useState("");
+
+    const { getTopicStats, stats, clearErrors, error, loading } =
+        useContext(JobContext);
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            clearErrors();
+        }
+    }, [error]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        getTopicStats(topic);
+    };
+
     return (
         <div className="modalMask">
             <div className="modalWrapper">
@@ -9,17 +31,23 @@ const TopicStats = () => {
                         <div className="headerWrapper">
                             <h3> Get Topic Stats </h3>
                         </div>
-                        <form className="form">
+                        <form className="form" onSubmit={submitHandler}>
                             <div className="inputWrapper">
                                 <div className="inputBox">
                                     <i aria-hidden className="fas fa-chart-line"></i>
-                                    <input type="text" placeholder="Enter Your Topic" required />
+                                    <input
+                                        type="text"
+                                        placeholder="Enter Your Topic"
+                                        value={topic}
+                                        onChange={(e) => setTopic(e.target.value)}
+                                        required
+                                    />
                                 </div>
                             </div>
 
                             <div className="uploadButtonWrapper">
                                 <button type="submit" className="uploadButton">
-                                    Get Stats
+                                    {loading ? "Fetching..." : "Get Stats"}
                                 </button>
                             </div>
                         </form>
@@ -27,44 +55,52 @@ const TopicStats = () => {
                 </div>
                 <div className="right">
                     <div className="rightContentWrapper">
-                        <h4>Stats of JAVA:</h4>
-                        <table className="table table-striped mt-4">
-                            <tbody>
-                                <tr>
-                                    <th scope="row">Average Positions</th>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Total Jobs</th>
-                                    <td>5</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Minimum Salary</th>
-                                    <td>75000</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Maximum Salary</th>
-                                    <td>75000</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Average Salary</th>
-                                    <td>75000</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {loading ? (
+                            <Loader />
+                        ) : stats && stats.message ? (
+                            <div className="alert alert-danger">
+                                <b>{stats.message}</b>
+                            </div>
+                        ) : (
+                            stats && (
+                                <>
+                                    <h4>Stats of {topic.toUpperCase()}:</h4>
+                                    <table className="table table-striped mt-4">
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">Average Positions</th>
+                                                <td>{stats.avg_positions}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Total Jobs</th>
+                                                <td>{stats.total_jobs}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Minimum Salary</th>
+                                                <td>${stats.min_salary}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Maximum Salary</th>
+                                                <td>${stats.max_salary}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Median Salary</th>
+                                                <td>${stats.avg_salary}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
 
-                        <div className="alert alert-danger mt-4">
-                            <b>Note:</b> These stats are collected from the jobs that are
-                            posted only on Jobire. Do not compare these stats with other
-                            sites.
-                        </div>
+                                    <div className="alert alert-danger mt-4">
+                                        <b>Note:</b> These stats are collected from the jobs that
+                                        are posted only on Jobire. Do not compare these stats with
+                                        other sites.
+                                    </div>
+                                </>
+                            )
+                        )}
                     </div>
                 </div>
             </div>
-            <div>
-                Stats by Jobire, 2022
-            </div>
-
         </div>
     );
 };
---
+++ frontend/pages/me/applied.js
@@ -0,0 +1,45 @@
+
+import axios from "axios";
+import Layout from "../../components/layouts/Layout";
+import JobsApplied from "../../components/job/JobsApplied";
+
+import { isAuthenticatedUser } from "../../utils/isAuthenticated";
+
+export default function JobsAppliedPage({ jobs }) {
+    console.log(jobs);
+    return (
+
+        <Layout title="Jobs Applied">
+            <JobsApplied jobs={jobs} />
+        </Layout>
+    );
+}
+
+export async function getServerSideProps({ req }) {
+    const access_token = req.cookies.access;
+
+    const user = await isAuthenticatedUser(access_token);
+
+    if (!user) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false,
+            },
+        };
+    }
+
+    const res = await axios.get(`${process.env.API_URL}/api/me/jobs/applied/`, {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+
+    const jobs = res.data;
+
+    return {
+        props: {
+            jobs,
+        },
+    };
+}
